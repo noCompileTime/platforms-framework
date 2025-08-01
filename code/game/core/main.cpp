7b57f49025bf9c2f077e191219268cd779623ade
iamdramaticleo <iamdramaticleo@gmail.com>
2025-08-01 14:39:26 +0300

    Add window events handling and enhance platform integration
    Introduced the `WindowEvents` base class to standardize event handling. Added integration of window creation, display, and destruction in the main function. Refactored and aligned core components, improving modularity and platform-specific extension support.

--- code/game/core/main.cpp
+++ code/game/core/main.cpp
@@ -1,4 +1,13 @@
+#include <core/platform.hpp>
+
 auto main() -> int
 {
+    auto window = core::Platform::create_factory()->create_window();
+
+    window->create({ }, { });
+    window->show();
+
+    window->destroy();
+
     return 0;
 }
